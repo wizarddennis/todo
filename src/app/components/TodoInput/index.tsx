e8857f22751cbb5ddca9b1fdf78ef3eb11973e99
DESKTOP-SRQ9MD1\Dennis <me.centric.world@gmail.com>
2023-12-27 17:54:55 +0900

    TodoInput 컴포넌트추가

---
+++ src/app/components/TodoInput/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  width: 100%auto;
+  font-size: 1.1em;
+  border-bottom: 1px solid #eee;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: none;
+  outline: 0;
+`;
+// 빈태그는 정의상 컴포넌트를 단순하게 코드상으로 연결하는 기능
+// import * as React from 'react'; 를 맨위에 넣지 않으면 아래 에러가 뜬다.
+// TS2686: 'React' refers to a UMD global, but the current file is a module. Consider adding an import instead.
+// 다른 파일에서 import 할수 있도록 export default 달아줌.
+export default function TodoInput() {
+  return (
+    <Box>
+      <Input placeholder="여기다가 할일을 입력하세요." />
+    </Box>
+  );
+}
