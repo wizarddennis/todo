c1a4507c98495b0bc61457b7b62f6b7c658e4956
DESKTOP-SRQ9MD1\Dennis <me.centric.world@gmail.com>
2024-01-02 15:24:55 +0900

    open graph 태그 사용까지 적용함

--- src/app/components/TodoItem/index.tsx
+++ src/app/components/TodoItem/index.tsx
@@ -13,11 +13,11 @@ const Box = styled.div`
 `;
 
 // 컨텐츠가 길어지면 자동으로 다음줄로 넘어가도록 하는 기능(cursor 위까지),
-// clamp3은 3줄이상되면 점으로 표시해라
+// clamp 3은 text-overflow가 ellipsis이니, 3줄이상되면 점으로 표시해라
 const TodoContent = styled.span<{ checked: boolean }>`
   overflow: hidden; //
   text-overflow: ellipsis;
-  work-wrap: break-work;
+  word-wrap: break-work;
   display: -webkit-box;
   -webkit-line-clamp: 3;
   -webkit-box-orient: vertical;
@@ -26,6 +26,7 @@ const TodoContent = styled.span<{ checked: boolean }>`
   color: ${props => (props.checked ? '#aaa' : '#212121')};
 `;
 
+// toDo 는 여러개이므로, 상위 컴포넌트에서 주는 걸 받아서 처리해야 함.
 export default function TodoItem({ todo }: { todo: ITodoItem }) {
   return (
     <Box>
